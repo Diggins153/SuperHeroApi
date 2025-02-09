7e5ee82a5993bfa83ca45b128a8fa0a025691826
dylan <dylanhabis@yahoo.com>
2025-02-09 18:12:12 +0000

    Add project files.

---
+++ SuperHeroApi/Models/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroApi.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        public string HeroName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
